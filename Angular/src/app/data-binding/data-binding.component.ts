e2b5955f85718868ea7a71d3f80f1ef61c08ad4f
lluanps <lluanps@gmail.com>
2023-05-17 01:29:25 -0300

    add exemplos de data bindings

--- Angular/src/app/data-binding/data-binding.component.ts
+++ Angular/src/app/data-binding/data-binding.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
   styleUrls: ['./data-binding.component.css']
 })
-export class DataBindingComponent {
+export class DataBindingComponent implements OnInit{
+ 
+  url: any = 'http://loiane.com';
+  cursoAngular: boolean = true;
+
+  getValor() {
+    return 1
+  }
+
+  getCurtirCurso() {
+    return true
+  }
+
+  ngOnInit() {
+  }
 
 }
