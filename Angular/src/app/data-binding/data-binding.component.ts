f88e7d0ef9dd7a620ad2c8d622ea307451f5464b
lluanps <lluanps@gmail.com>
2023-05-17 01:44:39 -0300

    property binding e interpolação

--- Angular/src/app/data-binding/data-binding.component.ts
+++ Angular/src/app/data-binding/data-binding.component.ts
@@ -9,6 +9,7 @@ export class DataBindingComponent implements OnInit{
  
   url: any = 'http://loiane.com';
   cursoAngular: boolean = true;
+  urlImagem: any = 'https://media.istockphoto.com/id/1226498373/pt/foto/herc%C3%ADlio-luz-bridge-in-florian%C3%B3polis-brazil.jpg?s=612x612&w=0&k=20&c=JANDN3-o2GUX9y5qNq25-Dyp5Nk1DiOSpIAMLI0UC2E='
 
   getValor() {
     return 1
